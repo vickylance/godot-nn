8f276e0fb619cf83c996842d7e0f7135eb205619
Vignesh S <vignesh.s1@thoughtworks.com>
2020-08-02 18:59:50 +0530

    camera follow the farthest bird

--- src/flappy_bird/scenes/Stage/Camera2D.gd
+++ src/flappy_bird/scenes/Stage/Camera2D.gd
@@ -1,9 +1,8 @@
 extends Camera2D
 
-onready var bird := get_tree().root.find_node("Bird", true, false) as Bird
-
 func _physics_process(_delta: float) -> void:
-	position = Vector2(bird.position.x, position.y)
+	if Game.bird:
+		position = Vector2(Game.bird.position.x, position.y)
 	pass
 
 
